551021cb54180aa5ab5dc5045a3b9baa70e1baf4
tycannon1 <cannont41@gmail.com>
2024-05-13 19:51:43 -0600

    second

--- place.js
+++ place.js
@@ -33,3 +33,11 @@ function displayWindChill() {
 
 
 window.onload = displayWindChill;
+
+const currentYear = new Date().getFullYear();
+  
+document.getElementById("copyright").innerHTML = `&copy; ${currentYear} Ty Cannon, USA`;
+  
+const lastModifiedDate = document.lastModified;
+  
+document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
