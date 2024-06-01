b439bed75b28bc8bf366a69a031c84038bfaac38
Ty Cannon <cannont41@gmail.com>
2024-06-01 07:20:35 -0600

    thrift update

--- project/scripts/thrift.js
+++ project/scripts/thrift.js
@@ -17,3 +17,27 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
     document.querySelector('.close-icon').classList.toggle('show');
 });
+fetch('data/thrift.json')
+        .then(response => response.json())
+        .then(data => {
+            displayMembers(data);
+        })
+        .catch(error => console.error('Error fetching member data:', error));
+
+    function displayMembers(members) {
+        members.forEach(member => {
+            const memberCard = document.createElement('div');
+            memberCard.classList.add('card');
+            memberCard.innerHTML = `
+            <img src="images/${member.image}" alt="${member.name}">
+            <div>
+                <h2>${member.name}</h2>
+                <p>${member.price}</p>
+                <p>${member.color}</p>
+                <p>${member.size}</p>
+             
+            </div>
+            `;
+            directory.appendChild(memberCard);
+        });
+    }
